39c0302e80be08c13f2c325bc2b4b32db2cbc651
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-06-27 17:59:37 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,165 @@
+package Learn_Java;
+
+import java.util.Scanner;
+
+public class Operators {
+
+	public static void main(String[] args) {
+//		Bitwise Operator
+		
+//		int x = 90;
+//		int y = 85;
+//		System.out.println(x&y);
+//		System.out.println(x|y);
+//		System.out.println(x^y);
+////		-(n+1)=>-(90+1)=>-91
+//		System.out.println(~x);
+//		System.out.println(~-x);
+		
+//		Unary Operator
+		
+//		int a = 10;
+//		System.out.println(a);
+////		inc
+//		System.out.println(++a);
+//		System.out.println(a++);
+//		System.out.println(a);
+////		dec
+//		System.out.println(--a);
+//		System.out.println(a--);
+//		System.out.println(a);		
+		
+		
+		
+		
+//		Ternary operator
+//		int b = 5;
+//		String v = (b==0)?"b is zero":"b is not zero";
+//		System.out.println(v);
+		
+		
+//		Conditioal statement/selection control
+//		1.if-else
+//		int b=5;
+//		if(b==0) {
+//			System.out.println("b is zero....");
+//		}
+//		else {
+//			System.out.println("b is not zero");
+//		}
+//		
+////		odd/even
+//		int n = 6;
+//		if(n%2 == 0) {
+//			System.out.println("n is even number");
+//		}
+//		else {
+//			System.out.println("n is odd number");
+//		}
+		
+//		leap year
+		
+//		n = 2024;
+//		if(n%4 == 0) {
+//			System.out.println(n+" is leap year");
+//		}
+//		else {
+//			System.out.println(n+" is not leap year");
+//		}	
+		
+		
+//		else if
+		
+//		String m = "may";
+//		if(m.equals("june")||m.equals("april")||m.equals("september")||m.equals("november")) {
+//			System.out.println(m+" is 30 days");
+//		}
+//		else if(m.equals("feb")) {
+//			System.out.println(m+" is 28 days");
+//		}
+//		else {
+//			System.out.println(m+" is 31 days");
+//		}
+		
+		
+//		Nested if
+		
+//		String email = "poornaa@gmail.com";
+//		if(email.equalsIgnoreCase("POORNA@GMAIL.COM")) {
+//			String pas = "pr123";
+//			if(pas.equals("pr123")) {
+//				System.out.println("Login Success😊👍..");
+//			}
+//			else {
+//				System.out.println("Invaild Password...");
+//			}
+//			
+//		}
+//		else {
+//			System.out.println("Invaild Email id...");
+//		}
+		
+//		switch-case
+//		int d = 0;
+//		switch(d) {
+//		case 1: System.out.println("Monday");break;
+//		case 2: System.out.println("Tuesday");break;
+//		case 3: System.out.println("Wednesday");break;
+//		case 4: System.out.println("Thursday");break;
+//		case 5: System.out.println("Friday");break;
+//		case 6: System.out.println("Saturday");break;
+//		case 7: System.out.println("Sunday");break;
+//		default:System.out.println("Invaild day");
+//		}
+		
+		
+		
+		
+//		inputs
+//		Scanner variable = new Scanner(System.in);
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the Name : ");
+//		String name = scan.next();
+		String name = scan.nextLine();
+		System.out.println("Name : "+name);
+		
+		System.out.println("Enter the Age : ");
+		int age = scan.nextInt();
+		System.out.println("Age : "+age);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
